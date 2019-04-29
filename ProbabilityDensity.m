6ddf3f70f6ee0c57973aef88972e2af2efa4b4ad
eMacqueene <50028649+eMacqueene@users.noreply.github.com>
2019-04-29 12:40:24 -0600

    Delete ProbabilityDensity.m

--- ProbabilityDensity.m
+++
@@ -1,9 +0,0 @@
-%    f(z)=12??e?z2/2
-z = [-5:1:5]
-f = (1/(sqrt(2*pi)))*(exp(1).^((-z.^2)/2))
-% need the dot because operators used are element by element on z.
-
-plot(z,f)
-xlabel('z')
-ylabel('Frequency')
-title('z vs Frequency')
