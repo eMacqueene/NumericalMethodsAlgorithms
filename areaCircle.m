40c81ccde32e5bcc138476181096a06d4cb9eda2
eMacqueene <50028649+eMacqueene@users.noreply.github.com>
2019-04-29 16:32:56 -0600

    Delete areaCircle.m

--- areaCircle.m
+++
@@ -1,9 +0,0 @@
-function[d] = dist(vo*t + (0.5 * a * t^2))
-mi2km = 1 / 0.621;
-hr2s = 3600;
-km2m = 1000;
-vo = vo * mi2km * km2m / hr2s;
-t = t * hr2s;
-d = vo*t + 0.5 * a * t^2;
-d = d/km2m;
-end
