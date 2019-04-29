e8a2a840a8ebb670a4e0341db0397d9c98e6fa20
eMacqueene <50028649+eMacqueene@users.noreply.github.com>
2019-04-29 12:21:00 -0600

    Delete dipshit.m

--- dipshit.m
+++
@@ -1,9 +0,0 @@
-
-TF = 32:3.6:93.2;
-TC = 5/9*(TF-32);
-rho = 5.5289e-8*TC.^3-8.5016e-6*TC.^2+6.5622e-5*TC+0.99987
-
-plot(rho,TC)
-title('Density vs Celsius')
-ylabel('Density (in grams per cubic centimeter)')
-xlabel('Temperature in Degrees Celsius')
